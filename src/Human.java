67c88081c0cab14cec3aa01a236ed7ffade9c22c
Андрей <honor10love2@gmail.com>
2022-12-18 15:47:29 +0300

    Переделал класс human

--- src/Human.java
+++ src/Human.java
@@ -3,12 +3,75 @@ public class Human {
     String name;
     String town;
     String jobTitle;
-    Human(int yearOfBirth, String name, String town, String jobTitle){
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
+    Human(int yearOfBirth){
+        this(yearOfBirth,"Информация не указанна");
+    }
+    Human(int yearOfBirth,String name){
+        this(yearOfBirth,name,"Информация не указанна");
+
+    }
+    Human(int yearOfBirth,String name,String town){
+        this(yearOfBirth,name,town,"Информация не указанна");
+
+    }
+
+   Human(int yearOfBirth, String name, String town, String jobTitle){
+
+       if(yearOfBirth>0) {
+           this.yearOfBirth = yearOfBirth;
+       }else {
+           this.yearOfBirth = 0;
+       }
+       if(name!=null){
+           this.name = name;
+       }else {
+           this.name = "Информация не указанна";
+       }
+       if(town!=null){
+           this.town = town;
+       }else {
+           this.town = "Информация не указанна";
+       }
+       if (jobTitle!= null) {
+           this.jobTitle = jobTitle;
+       }else {
+           this.jobTitle = "Информация не указанна";
+       }
+
+
+    }
+    /*
+    Human(int yearOfBirth,String town,String jobTitle){
+        if(yearOfBirth>0) {
+            this.yearOfBirth = yearOfBirth;
+        }else {
+            this.yearOfBirth = 0;
+        }
+        this.name = "Информация не указанна";
         this.town = town;
         this.jobTitle = jobTitle;
+
     }
+    Human(int yearOfBirth,String name,String jobTitle){
+        if(yearOfBirth>0) {
+            this.yearOfBirth = yearOfBirth;
+        }else {
+            this.yearOfBirth = 0;
+        }
+            this.name = name;
+            this.jobTitle = jobTitle;
+            this.town = "Информация не указана";
+        }/*
+
+
+
+
+    /*Продолжим работать с классом human.
+
+Дополните созданные ранее конструкторы проверками, которые
+позволяют проверить, что год рождения не может быть отрицательным, или ставят значение по умолчанию, равное нулю.
+   Если не указано имя, город проживания или должность, то есть передана пустая строка или null, то программа
+    должна подставить значение «Информация не указана» при выводе в консоль.*/
 
 
     void printHuman(){
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human max = new Human(1988, "Максим", "Минск", "Бренд-менеджер");
-        Human any = new Human(1993, "Аня", "Москва", "Методист образовательных программ");
-        Human katy = new Human(1992, "Катя","Калининград", "Продакт-менеджер");
-        Human artem = new Human(1995, "Артем", "Москва", "Директор по развитию бизнеса");
+        Human max = new Human(1988,null, "Минск", "Бренд-менеджер");
+        Human any = new Human(1993, "Аня", null, "Методист образовательных программ");
+        Human katy = new Human(1992, "Катя","Калининград", null);
+        Human artem = new Human(1995, null, null, null);
 
 
 
