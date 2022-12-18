58676251065f001d755d8aeab31d350adcd72376
Андрей <honor10love2@gmail.com>
2022-12-18 13:17:19 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Human max = new Human();//1988, "Максим", "Минск", "Бренд-менеджер"
+       /* Human any = new Human(1993, "Аня", "Москва", "Методист образовательных программ");
+        Human katy = new Human(1992, "Катя","Калининград", "Продакт-менеджер");
+        Human artem = new Human(1995, "Артем", "Москва", "Директор по развитию бизнеса");*/
+        max.name = "Mаксим";
+        max.yearOfBirth = 1988;
+        max.jobTitle = "Бренд-менеджер";
+        max.town = "Минск";
+
+
+
+        max.printHuman();
+      /*  any.printHuman();
+        katy.printHuman();
+        artem.printHuman();*/
+
+
+
+    }
+}
